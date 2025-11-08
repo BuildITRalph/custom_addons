724236fcbe9efbb1f5cc6a4b49a1a02065f76f94
Ralph Talplacido <ralph.sabalo@gmail.com>
2025-11-08 18:44:51 +0800

    add access rights for admin/user

--- dropship_vendor/__manifest__.py
+++ dropship_vendor/__manifest__.py
@@ -1,5 +1,5 @@
 {
-    'name': "PREFERRED VENDOR DROPSHIP",
+    'name': "DROPSHIP PREFERRED VENDOR",
 
     'summary': "Setup the Preferred Vendor in Dropship Orders",
 
@@ -14,15 +14,16 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Sales',
+    'category': 'Supply Chain',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base','sale','purchase','sale_stock','stock','stock_dropshipping'],
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/preferred_vendor_security.xml',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
     ],
@@ -30,5 +31,8 @@
     'demo': [
         'demo/demo.xml',
     ],
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': False,
 }
 
